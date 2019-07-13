9c6c193793220a510d7c4705d9ab09b9cbe80127
Evan Plett <evanplett@gmail.com>
2019-07-12 20:28:38 -0400

    Adding get response

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import app from './app';
 const PORT = process.env.PORT || '5000';
 
+app.get('/', (req, res) => res.send('Hello World!'));
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
