a08bfd290792df014311703bea5a197a20a18b88
Evan Plett <evanplett@gmail.com>
2019-08-07 22:06:58 -0400

    Rest App again

--- src/rest/app.ts
+++ src/rest/app.ts
@@ -2,14 +2,9 @@ import {Routes} from "./routes";
 import * as express from "express";
 import { MapRoutesOnApp } from "../routing_utils";
 
-const app = express();
-
-export class RestApp extends express.Application
-{
-  constructor() {
-     super();
-     MapRoutesOnApp(this, Routes);
-  }
+export default function RestApp() {
+     const app = express();
+     MapRoutesOnApp(app, Routes);
 }
 
 //MapRoutesOnApp(app, Routes);
