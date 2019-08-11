f33a6b806dd4b1af785ea61ad5c47efac299010f
Evan Plett <evanplett@gmail.com>
2019-08-11 08:11:25 -0400

    Simplifying test data creation

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,33 @@ const PORT = process.env.PORT || "5000";
 EnsureConnection().then(async connection => {
 
     // put some values in the database
-    FillWithTestData(connection);
+    //FillWithTestData(connection);
+
+
+let p1 = connection.manager.create( Statement, { text: "Premis 1" });
+		    let p2 = connection.manager.create( Statement, { text: "Premis 1" });
+		    let conc = connection.manager.create( Statement, { text: "Conclusion" }); 
+		    
+		    await connection.manager.save(p1);
+		    await connection.manager.save(p2);
+		    await connection.manager.save(conc);
+		    
+		    
+		    let a1 = connection.manager.create( Argument,
+		       {
+		          conclusion: conc//,
+		          premises: [p1, p2]
+		       });
+		  
+		    await connection.manager.save(a1);
+
+
+
+
+
+
+
+
 
     // create express app
     const app = express();
