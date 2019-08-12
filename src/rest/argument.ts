0d622cf0e55356580dde011a8cd3685dc672c3ad
Evan Plett <evanplett@gmail.com>
2019-08-12 19:44:15 -0400

    Trying to use query in rest handling

--- src/rest/argument.ts
+++ src/rest/argument.ts
@@ -11,7 +11,7 @@ export class Argument {
 
 
    async all(request: Request, response: Response, next: NextFunction) {
-        return this.af.getList(request.params.limit, request.params.after_id)
+        return this.af.getList(request.query.limit, request.query.after_id)
     }
     
     
