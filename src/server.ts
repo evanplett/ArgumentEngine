c32c489e6a9ebcb0e9144f1ef85ef3cba051939b
Evan Plett <evanplett@gmail.com>
2019-08-12 17:52:40 -0400

    Adding more Arguments

--- src/server.ts
+++ src/server.ts
@@ -11,33 +11,6 @@ import {Statement} from "./business_model_typeorm/entity/Statement";
 const PORT = process.env.PORT || "5000";
 
 EnsureConnection().then(async connection => {
-
-    // put some values in the database
-let p1 = connection.manager.create( Statement, { text: "Premis 1" });
-		    let p2 = connection.manager.create( Statement, { text: "Premis 2" });
-		    let conc = connection.manager.create( Statement, { text: "Conclusion" }); 
-		    
-		    await connection.manager.save(p1);
-		    await connection.manager.save(p2);
-		    await connection.manager.save(conc);
-		    
-		    
-		    let a1 = connection.manager.create( Argument,
-		       {
-		          conclusion: conc,
-		          premises: [p1, p2]
-		       });
-		  
-		    await connection.manager.save(a1);
-
-
-
-
-
-
-
-
-
     // create express app
     const app = express();
     app.use(bodyParser.json());
@@ -49,5 +22,26 @@ let p1 = connection.manager.create( Statement, { text: "Premis 1" });
     app.listen(PORT);
     
     console.log("Express server has started on port " + PORT + ".");
-
+    
+    
+    
+    for(let i = 0; i < 100; i++) {
+    
+    // put some values in the database
+    let p1 = connection.manager.create( Statement, { text: `Premis ${i}a` });
+		    let p2 = connection.manager.create( Statement, { text: `Premis ${i}b` });
+		    let conc = connection.manager.create( Statement, { text: `Conclusion ${i}`}); 
+		    
+		    await connection.manager.save(p1);
+		    await connection.manager.save(p2);
+		    await connection.manager.save(conc);
+		    
+		    let a1 = connection.manager.create( Argument,
+		       {
+		          conclusion: conc,
+		          premises: [p1, p2]
+		       });
+		  
+		    await connection.manager.save(a1);
+    }
 }).catch(error => console.log(error));
