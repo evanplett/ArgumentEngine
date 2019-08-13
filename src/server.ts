7f88b7150032fee658f64d70d680da93ab8bf74c
Evan Plett <evanplett@gmail.com>
2019-08-12 20:05:17 -0400

    Adding tree example

--- src/server.ts
+++ src/server.ts
@@ -24,10 +24,10 @@ EnsureConnection().then(async connection => {
     console.log("Express server has started on port " + PORT + ".");
     
     
-    
-    for(let i = 0; i < 100; i++) {
-    
     // put some values in the database
+    
+    // many
+    for(let i = 1; i < 100; i++) {
     let p1 = connection.manager.create( Statement, { text: `Premis ${i}a` });
 		    let p2 = connection.manager.create( Statement, { text: `Premis ${i}b` });
 		    let conc = connection.manager.create( Statement, { text: `Conclusion ${i}`}); 
@@ -44,4 +44,62 @@ EnsureConnection().then(async connection => {
 		  
 		    await connection.manager.save(a1);
     }
+    
+    
+    
+    // tree
+    
+    let t_p1a = connection.manager.create( Statement, { text: `Premis t1a` });
+		    let t_p1b = connection.manager.create( Statement, { text: `Premis t1b` });
+		    let t_conc1 = connection.manager.create( Statement, { text: `Conclusion t1`}); 
+		    
+		    await connection.manager.save(t_p1a);
+		    await connection.manager.save(t_p1b);
+		    await connection.manager.save(t_conc1);
+		    
+		    let t_a1 = connection.manager.create( Argument,
+		       {
+		          conclusion: t_conc1,
+		          premises: [t_p1a, t_p1b]
+		       });
+		  
+		    await connection.manager.save(t_a1);
+    
+    
+    
+    
+    let t_p2a = connection.manager.create( Statement, { text: `Premis t2a` });
+		    let t_p2b = connection.manager.create( Statement, { text: `Premis t2b` });
+		    let t_conc2 = connection.manager.create( Statement, { text: `Conclusion t2`}); 
+		    
+		    await connection.manager.save(t_p2a);
+		    await connection.manager.save(t_p2b);
+		    await connection.manager.save(t_conc2);
+		    
+		    let t_a2 = connection.manager.create( Argument,
+		       {
+		          conclusion: t_conc2,
+		          premises: [t_p2a, t_p2b]
+		       });
+		  
+		    await connection.manager.save(t_a2);
+    
+    
+    
+    
+    
+    
+		    let t_conc3 = connection.manager.create( Statement, { text: `Conclusion t3`}); 
+		    
+		 
+		    await connection.manager.save(t_conc3);
+		    
+		    let t_a3 = connection.manager.create( Argument,
+		       {
+		          conclusion: t_conc3,
+		          premises: [t_conc1, t_conc2]
+		       });
+		  
+		    await connection.manager.save(t_a3);
+    
 }).catch(error => console.log(error));
