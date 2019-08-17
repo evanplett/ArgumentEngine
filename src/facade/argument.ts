4f9bf280037756fe7ae821609f96ed0f868c3f3d
Evan Plett <evanplett@gmail.com>
2019-08-17 08:24:36 -0400

    Trying to fix list promise

--- src/facade/argument.ts
+++ src/facade/argument.ts
@@ -12,7 +12,7 @@ export class Argument
 {
   private ac = new ArgumentController();
 
-  async getList(limit?: number, after_id?: number): Promise<ModelArgument[]>
+  async getList(limit?: number, after_id?: number): Promise
   {
     limit = limit && limit > 0 ? limit : DEFAULT_LIMIT;
     
