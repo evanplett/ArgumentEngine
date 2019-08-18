ce1f6aaa433ab87ef63c585a40d64ff152b9dbe5
Evan Plett <evanplett@gmail.com>
2019-08-18 23:32:01 +0000

    Trying to fix

--- src/server.ts
+++ src/server.ts
@@ -5,38 +5,38 @@ import {Request, Response} from "express";
 import { RestApp } from "./rest/app";
 import { EnsureConnection } from "./business_model_typeorm/manager";
 
-import {Argument, ReasoningMethod} from "./business_model_typeorm/entity/Argument";
-import {Statement} from "./business_model_typeorm/entity/Statement";
+import {ModelArgument, ReasoningMethod} from "./business_model_typeorm/entity/Argument";
+import {ModelStatement} from "./business_model_typeorm/entity/Statement";
 
 const PORT = process.env.PORT || "5000";
 
-async function CreateNode(connection: any, max_level: number, current_level: number = 0,  path: string = ""): Promise<Statement> {
-   let conclusion = connection.manager.create( Statement, { text: "Conclusion " + path});
+async function CreateNode(connection: any, max_level: number, current_level: number = 0,  path: string = ""): Promise<ModelStatement> {
+   let conclusion = connection.manager.create( ModelStatement, { text: "Conclusion " + path});
 
     await connection.manager.save(conclusion);
-    
+
     if (current_level < max_level)
     {
        let leftNode = await CreateNode(connection, max_level, current_level + 1, path + "L");
 
        let rightNode = await CreateNode(connection, max_level, current_level + 1, path + "R");
 
-       let argument = connection.manager.create( Argument,
+       let argument = connection.manager.create( ModelArgument,
 		       {
 		          conclusion: conclusion,
 		          premises: [leftNode, rightNode],
 		          reasoningMethod: ReasoningMethod.Induction
 		       });
-		  
+
 		    await connection.manager.save(argument);
     }
-    
-    
-    return new Promise<Statement>((resolve) => {
+
+
+    return new Promise<ModelStatement>((resolve) => {
         resolve(conclusion);
         });
-    
-    
+
+
     //return conclusion;
 }
 
@@ -51,33 +51,33 @@ EnsureConnection().then(async connection => {
 
     // start express server
     app.listen(PORT);
-    
+
     console.log("Express server has started on port " + PORT + ".");
-    
-    
+
+
     // put some values in the database
-    
+
     // many
     for(let i = 1; i < 100; i++) {
-    let p1 = connection.manager.create( Statement, { text: `Premis ${i}a` });
-		    let p2 = connection.manager.create( Statement, { text: `Premis ${i}b` });
-		    let conc = connection.manager.create( Statement, { text: `Conclusion ${i}`}); 
-		    
+    let p1 = connection.manager.create( ModelStatement, { text: `Premis ${i}a` });
+		    let p2 = connection.manager.create( ModelStatement, { text: `Premis ${i}b` });
+		    let conc = connection.manager.create( ModelStatement, { text: `Conclusion ${i}`});
+
 		    await connection.manager.save(p1);
 		    await connection.manager.save(p2);
 		    await connection.manager.save(conc);
-		    
-		    let a1 = connection.manager.create( Argument,
+
+		    let a1 = connection.manager.create( ModelArgument,
 		       {
 		          conclusion: conc,
 		          premises: [p1, p2],
 		          reasoningMethod: ReasoningMethod.Deduction
 		       });
-		  
+
 		    await connection.manager.save(a1);
     }
-    
+
     // tree
     CreateNode(connection, 3);
-    
+
 }).catch(error => console.log(error));
