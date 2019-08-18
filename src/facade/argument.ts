00861514fce7373f3c6dce43f192035e5845468f
Evan Plett <evanplett@gmail.com>
2019-08-18 02:21:48 +0000

    Trying to put things back to working

--- src/facade/argument.ts
+++ src/facade/argument.ts
@@ -16,18 +16,13 @@ export class Argument {
 
         after_id = after_id && after_id >= 0 ? after_id : DEFAULT_AFTER_ID;
 
-        this.ac.many(after_id, limit).then(many => {
-            if (many.length > 0) {
-                return Promise.resolve(many);
-            } else {
-                return Promise.reject(`No Arguments after id ${after_id} found`);
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        });
+        let many = await this.ac.many(after_id, limit);
 
-        return Promise.reject('Internal server error');
+        if (many.length > 0) {
+            return Promise.resolve(many);
+        } else {
+            return Promise.reject(`No Arguments after id ${after_id} found`);
+        }
     }
 
     async getOne(id: number): Promise<ModelArgument> {
