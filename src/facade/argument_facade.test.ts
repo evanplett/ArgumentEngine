788815724257798a321c0aea8daeb77c2d7beec1
Evan Plett <evanplett@gmail.com>
2019-08-27 22:26:32 +0000

    Another attempt

--- src/facade/argument_facade.test.ts
+++ src/facade/argument_facade.test.ts
@@ -1,19 +1,21 @@
 import { FacadeArgument } from './argument_facade';
 import { EnsureConnection } from "../business_model_typeorm/manager";
 
+function Initialize(): boolean {
+    return EnsureConnection()
+        .then(connection => {
+            console.log("Connection Created");
+            this.af = new FacadeArgument();
+            return true;
+        })
+        .catch(error => {
+            return false;
+        });
+}
+
 describe('This is a simple test', () => {
     beforeAll(() => {
-        expect(() => {
-           return EnsureConnection()
-                .then(connection => {
-                    console.log("Connection Created");
-                    this.af = new FacadeArgument();
-                    return true;
-                })
-                .catch(error => {
-                    return false;
-                });
-        }).toBe(true);
+        expect(Initialize()).toBe(true);
     });
 
     test('Check the sampleFunction function', () => {
