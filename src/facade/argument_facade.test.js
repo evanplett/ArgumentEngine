39642a97def38be01b318f9607d96a7147f21b28
Evan Plett <evanplett@gmail.com>
2019-08-27 21:31:15 +0000

    Trying to get jest working

---
+++ src/facade/argument_facade.test.js
@@ -0,0 +1,13 @@
+import { FacadeArgument } from './argument_facade';
+import { EnsureConnection } from "../business_model_typeorm/manager";
+
+describe('This is a simple test', () => {
+	beforeAll(async () => {
+        await EnsureConnection();
+        this.af = new FacadeArgument();
+    });
+
+	test('Check the sampleFunction function', () => {
+		expect(this.af.getList()).toEqual('hellohello');
+	});
+});
