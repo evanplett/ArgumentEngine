b33f30c83ea1e45574de95edf98257e8a22ff57f
Evan Plett <evanplett@gmail.com>
2019-08-27 22:23:34 +0000

    Trying another thing

--- src/facade/argument_facade.test.ts
+++ src/facade/argument_facade.test.ts
@@ -2,13 +2,21 @@ import { FacadeArgument } from './argument_facade';
 import { EnsureConnection } from "../business_model_typeorm/manager";
 
 describe('This is a simple test', () => {
-	beforeAll(() => {
-        EnsureConnection();
-        console.log("Connection Created");
-        this.af = new FacadeArgument();
+    beforeAll(() => {
+        expect(() => {
+           return EnsureConnection()
+                .then(connection => {
+                    console.log("Connection Created");
+                    this.af = new FacadeArgument();
+                    return true;
+                })
+                .catch(error => {
+                    return false;
+                });
+        }).toBe(true);
     });
 
-	test('Check the sampleFunction function', () => {
-		expect(this.af.getList()).toEqual('hellohello');
-	});
+    test('Check the sampleFunction function', () => {
+        expect(this.af.getList()).toEqual('hellohello');
+    });
 });
