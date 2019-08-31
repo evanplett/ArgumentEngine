38adfe21ce47e452adf03f536a7aaef3da1df192
Evan Plett <evanplett@gmail.com>
2019-08-31 22:28:16 +0000

    Commenting out test stuff

---
+++ src/business_model_typeorm/controller/ArgumentController.test.ts
@@ -0,0 +1,2 @@
+import { ArgumentController } from './ArgumentController';
+
--- src/facade/argument_facade.test.ts
+++ src/facade/argument_facade.test.ts
@@ -1,24 +1,24 @@
-import { FacadeArgument } from './argument_facade';
-import { EnsureConnection } from "../business_model_typeorm/manager";
+// import { FacadeArgument } from './argument_facade';
+// import { EnsureConnection } from "../business_model_typeorm/manager";
 
-function Initialize(): boolean {
-    return EnsureConnection()
-        .then(connection => {
-            console.log("Connection Created");
-            this.af = new FacadeArgument();
-            return true;
-        })
-        .catch(error => {
-            return false;
-        });
-}
+// function Initialize(): boolean {
+//     return EnsureConnection()
+//         .then(connection => {
+//             console.log("Connection Created");
+//             this.af = new FacadeArgument();
+//             return true;
+//         })
+//         .catch(error => {
+//             return false;
+//         });
+// }
 
-describe('This is a simple test', () => {
-    beforeAll(() => {
-        expect(Initialize()).toBe(true);
-    });
+// describe('This is a simple test', () => {
+//     beforeAll(() => {
+//         expect(Initialize()).toBe(true);
+//     });
 
-    test('Check the sampleFunction function', () => {
-        expect(this.af.getList()).toEqual('hellohello');
-    });
-});
+//     test('Check the sampleFunction function', () => {
+//         expect(this.af.getList()).toEqual('hellohello');
+//     });
+// });
