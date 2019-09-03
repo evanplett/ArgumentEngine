484e80a1b86ce78187fa87e935dbaad00194fe55
Evan Plett <evanplett@gmail.com>
2019-09-03 21:37:14 +0000

    Trying a new thing

--- src/business_model_typeorm/manager.ts
+++ src/business_model_typeorm/manager.ts
@@ -1,4 +1,21 @@
-import {createConnection } from "typeorm";
+import { createConnection, Connection } from "typeorm";
 
-export { createConnection as EnsureConnection } from "typeorm";
+export class BusinessModelManager {
+    static connection: Connection;
+
+    static async EnsureConnection() : Connection {
+        if (!this.connection) {
+            this.connection = await createConnection();
+        }
+
+        return this.connection;
+    }
+
+    static CloseConnection() {
+        if (this.connection)
+        {
+            this.connection.close();
+        }
+    }
+}
 
--- src/facade/argument_facade.test.ts
+++ src/facade/argument_facade.test.ts
@@ -1,11 +1,19 @@
-import { EnsureConnection } from "../business_model_typeorm/manager";
+import { BusinessModelManager } from "../business_model_typeorm/manager";
 
 describe('This is a fake test', () => {
 
+    beforeAll(() => {
+        BusinessModelManager.EnsureConnection();
+    });
 
     test('Check the fake function', () => {
-        return EnsureConnection().then(async connection => {
-            expect(4).toEqual(4);
-        });
+        expect(4).toEqual(4);
+        // return EnsureConnection().then(async connection => {
+        //     expect(4).toEqual(4);
+        // });
+    });
+
+    afterAll(() => {
+        BusinessModelManager.CloseConnection();
     });
 });
