5e01dbc587a61d10519c28c61c18a52b078e4eb2
Evan Plett <evanplett@gmail.com>
2019-10-25 23:16:24 +0000

    Trying to add SuperTest Test

---
+++ src/apiTest.js
@@ -0,0 +1,19 @@
+//apiTest.js
+const request = require('supertest');
+const app = require('/rest/app'); //reference to you app.js file
+
+//==================== user API test ====================
+
+/**
+ * Testing get all user endpoint
+ */
+describe('GET /argument', function () {
+    it('respond with json containing a list of all users', function (done) {
+        request(app)
+            .get('/argument')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+
