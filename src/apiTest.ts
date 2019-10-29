858ea9b2cde99c289afb9cfebc400c7dbe94ad34
Evan Plett <evanplett@gmail.com>
2019-10-28 22:25:21 -0400

    fixing apiTest db reference

--- src/apiTest.ts
+++ src/apiTest.ts
@@ -24,7 +24,7 @@ describe('GET /argument', function () {
     });
 
     afterEach(() => {
-        let conn = getConnection();
+        let conn = getConnection("testing");
         return conn.close();
     })
 
