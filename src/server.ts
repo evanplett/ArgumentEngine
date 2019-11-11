c8c48c0d96ab2a4771e654ca5ac7057b4f20e236
Evan Plett <evanplett@gmail.com>
2019-11-11 09:54:08 -0500

    Adding another test file

--- src/server.ts
+++ src/server.ts
@@ -11,34 +11,6 @@ import {ModelStatement} from "./business_model_typeorm/entity/Statement";
 const PORT = process.env.PORT || "5000";
 
 
-async function CreateNode(connection: any, max_level: number, current_level: number = 0,  path: string = ""): Promise<ModelStatement> {
-   let conclusion = connection.manager.create( ModelStatement, { text: "Conclusion " + path});
-
-    await connection.manager.save(conclusion);
-
-    if (current_level < max_level)
-    {
-       let leftNode = await CreateNode(connection, max_level, current_level + 1, path + "L");
-
-       let rightNode = await CreateNode(connection, max_level, current_level + 1, path + "R");
-
-       let argument = connection.manager.create( ModelArgument,
-		       {
-		          conclusion: conclusion,
-		          premises: [leftNode, rightNode],
-		          reasoningMethod: ReasoningMethod.Induction
-		       });
-
-		    await connection.manager.save(argument);
-    }
-
-
-    return new Promise<ModelStatement>((resolve) => {
-        resolve(conclusion);
-        });
-}
-
-
 MyConnectionManager.SetCurrentConnection("production").then(async connection => {
     // create express app
     const app = express();
