1d72ceb48cdd3ee7ce7becf49fbad615b2c31136
Evan Plett <evanplett@gmail.com>
2019-11-21 07:33:14 -0500

    Adding some logging

--- src/rest/argument_rest.ts
+++ src/rest/argument_rest.ts
@@ -1,54 +1,61 @@
-import { NextFunction, Request, Response } from 'express';
+import {
+  NextFunction,
+  Request,
+  Response
+} from 'express';
 
-import { FacadeArgument } from '../facade/argument_facade';
+import {
+  FacadeArgument
+} from '../facade/argument_facade';
 
 export class Argument {
-    private af = new FacadeArgument();
+  private af = new FacadeArgument();
 
-    // *********** CREATE ********** //
-    async create(request: Request, response: Response, next: NextFunction) {
-         this.af
-            .createOne(request.body.conclusion, request.body.reasoning_method, request.body.premises)
-            .then((value) => {
-                response.status(200).json(value);
-            })
-            .catch((error) => {
-                response.status(400).send(error);
-            });
-    }
+  // *********** CREATE ********** //
+  async create(request: Request, response: Response, next: NextFunction) {
+    console.log("Rest::create");
+    this.af
+    .createOne(request.body.conclusion, request.body.reasoning_method, request.body.premises)
+    .then((value) => {
+      response.status(200).json(value);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+  }
 
-    // *********** READ ********** //
-    async many(request: Request, response: Response, next: NextFunction) {
-        this.af
-            .getList(request.query.limit, request.query.after_id)
-            .then((value) => {
-                response.status(200).json(value);
-            })
-            .catch((error) => {
-                response.status(400).send(error);
-            });
-    }
+  // *********** READ ********** //
+  async many(request: Request, response: Response, next: NextFunction) {
+    this.af
+    .getList(request.query.limit, request.query.after_id)
+    .then((value) => {
+      response.status(200).json(value);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+  }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        this.af
-            .getOne(request.params.id)
-            .then((value) => {
-                response.status(200).json(value);
-            })
-            .catch((error) => {
-                response.status(400).send(error);
-            });
-    }
+  async one(request: Request, response: Response, next: NextFunction) {
+    this.af
+    .getOne(request.params.id)
+    .then((value) => {
+      response.status(200).json(value);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+  }
 
 
-    async tree(request: Request, response: Response, next: NextFunction) {
-        this.af
-        .getTree(request.params.id, request.query.max_depth)
-        .then((value) =>{
-            response.status(200).json(value);
-        })
-        .catch((error) => {
-                response.status(400).send(error);
-         });
-    }
+  async tree(request: Request, response: Response, next: NextFunction) {
+    this.af
+    .getTree(request.params.id, request.query.max_depth)
+    .then((value) => {
+      response.status(200).json(value);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+  }
 }
