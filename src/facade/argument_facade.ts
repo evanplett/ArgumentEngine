bf8f56353436d00064720018f77af2cfbe0db360
Evan Plett <evanplett@gmail.com>
2019-12-10 18:13:47 -0500

    Fixing typo

--- src/facade/argument_facade.ts
+++ src/facade/argument_facade.ts
@@ -39,91 +39,91 @@ export class FacadeArgument {
     reasoning_method: string,
     premises: (string | number)[]
   ): Promise < ModelArgument > {
-    logger.trace`Façade::Argument::createOne(conclusion: ${conclusion} | reasoning_method: ${reasoning_method} | premises: ${premises})`);
-  let conclusionStatement: Promise < ModelStatement > =
-  typeof conclusion === 'string' ? this.fs.createOne(conclusion): this.fs.getOne(conclusion);
+    logger.trace(`Façade::Argument::createOne(conclusion: ${conclusion} | reasoning_method: ${reasoning_method} | premises: ${premises})`);
+    let conclusionStatement: Promise < ModelStatement > =
+    typeof conclusion === 'string' ? this.fs.createOne(conclusion): this.fs.getOne(conclusion);
 
-  let premisStatements: Promise < ModelStatement[] > = Promise.all(
-    premises.map(async (premis) => {
-      return typeof premis === 'string' ? this.fs.createOne(premis): this.fs.getOne(premis);
+    let premisStatements: Promise < ModelStatement[] > = Promise.all(
+      premises.map(async (premis) => {
+        return typeof premis === 'string' ? this.fs.createOne(premis): this.fs.getOne(premis);
+      })
+    );
+
+    let methodOfReasoning: Promise < ReasoningMethod > = ModelArgument.stringToReasoningMethod(reasoning_method);
+
+    return Promise.all([conclusionStatement,
+      premisStatements,
+      methodOfReasoning])
+    .then(([conclusionValue, premisValues, reasoningMethodValue]) => {
+      return this.ac.createOne(conclusionValue, reasoningMethodValue, premisValues);
     })
-  );
+    .catch((error) => {
+      return Promise.reject(new Error(400, `Unable to create Argument: ${error}`));
+    });
+  }
 
-  let methodOfReasoning: Promise < ReasoningMethod > = ModelArgument.stringToReasoningMethod(reasoning_method);
+  // *********** READ ********** //
+  getList(limit?: number,
+    after_id?: number): Promise < ModelArgument[] > {
+    logger.trace('Façade::Argument::getList');
+    limit = limit && limit > 0 ? limit: FacadeArgument.DEFAULT_LIMIT;
+    after_id = after_id && after_id >= 0 ? after_id: FacadeArgument.DEFAULT_AFTER_ID;
 
-  return Promise.all([conclusionStatement,
-    premisStatements,
-    methodOfReasoning])
-  .then(([conclusionValue, premisValues, reasoningMethodValue]) => {
-    return this.ac.createOne(conclusionValue, reasoningMethodValue, premisValues);
-  })
-  .catch((error) => {
-    return Promise.reject(new Error(400, `Unable to create Argument: ${error}`));
-  });
-}
-
-// *********** READ ********** //
-getList(limit?: number,
-  after_id?: number): Promise < ModelArgument[] > {
-  logger.trace('Façade::Argument::getList');
-  limit = limit && limit > 0 ? limit: FacadeArgument.DEFAULT_LIMIT;
-  after_id = after_id && after_id >= 0 ? after_id: FacadeArgument.DEFAULT_AFTER_ID;
-
-  return this.ac.many(after_id,
-    limit)
-  .then(list => {
-    if (list.length > 0) {
-      return Promise.resolve(list);
-    } else {
-      return Promise.reject(new Error(400, `No Arguments after id ${after_id} found`));
-    }
-  });
-}
-
-getOne(id: number): Promise < ModelArgument > {
-  logger.trace('Façade::Argument::getOne');
-  return this.ac.one(id)
-  .catch(error => {
-    return Promise.reject(new Error(400, `No Argument with id ${id} found`));
-  });
-}
-
-getTree(id: number,
-  max_depth?: number): Promise < ArgumentTreeNode > {
-  console.debug('Façade::Argument::getTree');
-  max_depth = max_depth && max_depth > 0 ? max_depth: FacadeArgument.DEFAULT_MAX_DEPTH;
-
-  return this.getTreeNode(id,
-    max_depth);
-}
-
-getTreeNode(id: number,
-  max_depth: number,
-  current_depth: number = 0): Promise < ArgumentTreeNode > {
-  logger.trace('Façade::Argument::getTreeNode');
-  return this.getOne(id)
-  .then(argument => {
-    if (current_depth >= max_depth) {
-      return {
-        argument_id: argument.id,
-        premises: [],
-        reasoning_method: argument.reasoning_method
+    return this.ac.many(after_id,
+      limit)
+    .then(list => {
+      if (list.length > 0) {
+        return Promise.resolve(list);
+      } else {
+        return Promise.reject(new Error(400, `No Arguments after id ${after_id} found`));
       }
-    } else {
-      return Promise.all(
-        argument.premises.map(statement =>
-          this.fs.getTreeNode(statement.id, max_depth, current_depth + 1)))
-      .then(children => {
+    });
+  }
+
+  getOne(id: number): Promise < ModelArgument > {
+    logger.trace('Façade::Argument::getOne');
+    return this.ac.one(id)
+    .catch(error => {
+      return Promise.reject(new Error(400, `No Argument with id ${id} found`));
+    });
+  }
+
+  getTree(id: number,
+    max_depth?: number): Promise < ArgumentTreeNode > {
+    console.debug('Façade::Argument::getTree');
+    max_depth = max_depth && max_depth > 0 ? max_depth: FacadeArgument.DEFAULT_MAX_DEPTH;
+
+    return this.getTreeNode(id,
+      max_depth);
+  }
+
+  getTreeNode(id: number,
+    max_depth: number,
+    current_depth: number = 0): Promise < ArgumentTreeNode > {
+    logger.trace('Façade::Argument::getTreeNode');
+    return this.getOne(id)
+    .then(argument => {
+      if (current_depth >= max_depth) {
         return {
           argument_id: argument.id,
-          premises: children,
+          premises: [],
           reasoning_method: argument.reasoning_method
         }
-      });
-    }
-  });
+      } else {
+        return Promise.all(
+          argument.premises.map(statement =>
+            this.fs.getTreeNode(statement.id, max_depth, current_depth + 1)))
+        .then(children => {
+          return {
+            argument_id: argument.id,
+            premises: children,
+            reasoning_method: argument.reasoning_method
+          }
+        });
+      }
+    });
 
-  /*
+    /*
                 let arg = await this.getOne(id);
 
                 let children =
@@ -140,5 +140,5 @@ getTreeNode(id: number,
                     premises: children,
                     reasoning_method: arg.reasoningMethod
                 };*/
-}
+  }
 }
