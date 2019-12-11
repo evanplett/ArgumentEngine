3dd1b81ea0baa400704385820f2eb808cf701f84
Evan Plett <evanplett@gmail.com>
2019-12-11 02:26:52 +0000

    Adding first filled in DB test

--- src/test/int/rest/argument_rest.int.test.ts
+++ src/test/int/rest/argument_rest.int.test.ts
@@ -1,188 +1,166 @@
-import {
-  createConnection,
-  getConnection,
-  Entity,
-  getRepository
-} from 'typeorm';
+import { createConnection, getConnection, Entity, getRepository } from 'typeorm';
 
+import { MyConnectionManager } from '../../../business_model_typeorm/manager';
 
-import {
-  MyConnectionManager
-} from '../../../business_model_typeorm/manager';
+import { ModelArgument, ReasoningMethod } from '../../../business_model_typeorm/entity/Argument';
+import { ModelStatement } from '../../../business_model_typeorm/entity/Statement';
 
-import {
-  ModelArgument
-} from '../../../business_model_typeorm/entity/Argument';
-import {
-  ModelStatement
-} from '../../../business_model_typeorm/entity/Statement';
-
-import {
-  TestUtils,
-  ArgumentParams
-} from './test_utils';
-
+import { TestUtils, ArgumentParams } from './test_utils';
 
 import * as request from 'supertest';
 
-// const request = require('supertest');
-
-
-import {
-  RestApp
-} from '../../../rest/app';
+import { RestApp } from '../../../rest/app';
 const app = RestApp();
 
+async function CreateNode(
+	connection: any,
+	max_level: number,
+	current_level: number = 0,
+	path: string = ''
+): Promise<ModelStatement> {
+	let conclusion = connection.manager.create(ModelStatement, { text: 'Conclusion ' + path });
 
-describe('With an empty database', function (): void {
+	await connection.manager.save(conclusion);
 
-  beforeEach(() => {
-    return MyConnectionManager.SetCurrentConnection('testing');
-  });
+	if (current_level < max_level) {
+		let leftNode = await CreateNode(connection, max_level, current_level + 1, path + 'L');
 
-  afterEach(() => {
-    let conn = MyConnectionManager.GetCurrentConnection();
-    return conn.close();
-  });
+		let rightNode = await CreateNode(connection, max_level, current_level + 1, path + 'R');
 
-  describe ('GET Argument', function (): void {
-    it('with no parameters, respond with code 400 and error message', function (): any {
-      return request(app)
-      .get('/argument')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(400, {
-        errorCode: 400,
-        errorDetail: 'No Arguments after id 0 found'
-      });
-    });
-    it('with after_id = 10, respond with code 400 and error message', function (): any {
-      return request(app)
-      .get('/argument')
-      .query({
-        after_id: '10'
-      })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(400, {
-        errorCode: 400,
-        errorDetail: 'No Arguments after id 10 found'
-      });
-    });
-    it('with limit = 10, respond with code 400 and error message', function (): any {
-      return request(app)
-      .get('/argument')
-      .query({
-        limit: '10'
-      })
-      .type('json')
-      .accept('json')
-      .expect(400, {
-        errorCode: 400,
-        errorDetail: 'No Arguments after id 0 found'
-      });
-    });
-    it('with after_id = 10 and limit = 10, respond with code 400 and error message', function (): any {
-      return request(app)
-      .get('/argument')
-      .query({
-        limit: '10',
-        after_id: '10'
-      })
-      .type('json')
-      .accept('json')
-      .expect(400, {
-        errorCode: 400,
-        errorDetail: 'No Arguments after id 10 found'
-      });
-    });
+		let argument = connection.manager.create(ModelArgument, {
+			conclusion: conclusion,
+			premises: [ leftNode, rightNode ],
+			reasoningMethod: ReasoningMethod.Induction
+		});
 
-    it('tree with id = 0 and max_depth = 10, respond with code 400 and error message', function (): any {
-      let id: number = 0;
+		await connection.manager.save(argument);
+	}
 
-      return request(app)
-      .get(`/argument/${id}/tree`)
-      .query({
-        max_depth: '10'
-      })
-      .type('json')
-      .accept('json')
-      .expect(400, {
-        errorCode: 400,
-        errorDetail: 'No Argument with id 0 found'
-      });
-    });
+	return new Promise<ModelStatement>((resolve) => {
+		resolve(conclusion);
+	});
+}
 
-  });
+describe('With an empty database', function(): void {
+	beforeEach(() => {
+		return MyConnectionManager.SetCurrentConnection('testing');
+	});
 
-  describe ('POST Argument', function (): void {
-    it('with valid new argument, respond with code 200 and error message', function (): any {
+	afterEach(() => {
+		let conn = MyConnectionManager.GetCurrentConnection();
+		return conn.close();
+	});
 
-      let newArg = new ArgumentParams(
-        'My Conclusion',
-        ['Premise 1', 'Premise 2'],
-        'Induction'
-      );
+	describe('GET Argument', function(): void {
+		it('with no parameters, respond with code 400 and error message', function(): any {
+			return request(app)
+				.get('/argument')
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Arguments after id 0 found'
+				});
+		});
+		it('with after_id = 10, respond with code 400 and error message', function(): any {
+			return request(app)
+				.get('/argument')
+				.query({
+					after_id: '10'
+				})
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Arguments after id 10 found'
+				});
+		});
+		it('with limit = 10, respond with code 400 and error message', function(): any {
+			return request(app)
+				.get('/argument')
+				.query({
+					limit: '10'
+				})
+				.type('json')
+				.accept('json')
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Arguments after id 0 found'
+				});
+		});
+		it('with after_id = 10 and limit = 10, respond with code 400 and error message', function(): any {
+			return request(app)
+				.get('/argument')
+				.query({
+					limit: '10',
+					after_id: '10'
+				})
+				.type('json')
+				.accept('json')
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Arguments after id 10 found'
+				});
+		});
 
-      return request(app)
-      .post('/argument')
-      .type('json')
-      .send(newArg)
-      .accept('json')
-      .expect(200)
-      .expect((res) => {
+		it('tree with id = 0 and max_depth = 10, respond with code 400 and error message', function(): any {
+			let id: number = 0;
 
-        let errors: string[] = TestUtils.DoesArgumentMatchArgElements(newArg, res.body);
+			return request(app)
+				.get(`/argument/${id}/tree`)
+				.query({
+					max_depth: '10'
+				})
+				.type('json')
+				.accept('json')
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Argument with id 0 found'
+				});
+		});
+	});
 
-        if (errors.length > 0) throw new Error(errors.map(error => '\n - ' + error).join(''));
-      });
-      /*.end((err, res) => {
+	describe('POST Argument', function(): void {
+		it('with valid new argument, respond with code 200 and error message', function(): any {
+			let newArg = new ArgumentParams('My Conclusion', [ 'Premise 1', 'Premise 2' ], 'Induction');
+
+			return request(app).post('/argument').type('json').send(newArg).accept('json').expect(200).expect((res) => {
+				let errors: string[] = TestUtils.DoesArgumentMatchArgElements(newArg, res.body);
+
+				if (errors.length > 0) throw new Error(errors.map((error) => '\n - ' + error).join(''));
+			});
+			/*.end((err, res) => {
         if (err) {
           return err;
         }
         expect(TestUtils.DoesArgumentMatchArgElements(newArg, res.body)).to.eql([]);
       });*/
-    });
-  });
-
+		});
+	});
 });
 
+describe('With an filled-in database', function(): void {
+	beforeEach(() => {
+		return MyConnectionManager.SetCurrentConnection('testing').then((connection) => {
+			CreateNode(connection, 4);
+		});
+	});
 
+	afterEach(() => {
+		let conn = MyConnectionManager.GetCurrentConnection();
+		return conn.close();
+	});
 
-
-
-
-
-/*
-
-
-
-*/
-
-
-/*async function CreateNode(connection: any, max_level: number, current_level: number = 0,  path: string = ''): Promise<ModelStatement> {
-   let conclusion = connection.manager.create( ModelStatement, { text: 'Conclusion ' + path});
-
-    await connection.manager.save(conclusion);
-
-    if (current_level < max_level)
-    {
-       let leftNode = await CreateNode(connection, max_level, current_level + 1, path + 'L');
-
-       let rightNode = await CreateNode(connection, max_level, current_level + 1, path + 'R');
-
-       let argument = connection.manager.create( ModelArgument,
-		       {
-		          conclusion: conclusion,
-		          premises: [leftNode, rightNode],
-		          reasoningMethod: ReasoningMethod.Induction
-		       });
-
-		    await connection.manager.save(argument);
-    }
-
-
-    return new Promise<ModelStatement>((resolve) => {
-        resolve(conclusion);
-        });
-}*/
+	describe('GET Argument', function(): void {
+		it('with no parameters, respond with code 400 and error message', function(): any {
+			return request(app)
+				.get('/argument')
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(400, {
+					errorCode: 400,
+					errorDetail: 'No Arguments after id 0 found'
+				});
+		});
+	});
+});
