0583455ac223bd1297c15441d6cabb1e96ee886d
Evan Plett <evanplett@gmail.com>
2019-12-11 02:00:00 +0000

    Setting everything back to trace

--- src/log_config.ts
+++ src/log_config.ts
@@ -7,7 +7,7 @@ import {
 } from 'typescript-logging';
 
 
-CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Warn));
+CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Trace));
 
 export const logArgumentEngineBase = new Category('argument_engine');
 
