c6325eba0149831be061d6239fb6a6356e2e4728
Evan Plett <evanplett@gmail.com>
2020-01-01 18:09:30 +0000

    More updates to serializer

--- src/facade/jsonapi_serializers.ts
+++ src/facade/jsonapi_serializers.ts
@@ -7,9 +7,14 @@ export const ArgumentSerializer: Serializer = new Serializer('Argument', {
         ref: 'id',
         attributes: ['text']
     },
+    premises: {
+        ref: 'id',
+        attributes: ['text']
+    },
     typeForAttribute: attribute => {
         switch (attribute) {
             case 'conclusion':
+            case 'premises':
                 return 'Statement';
             default:
                 return undefined;
