4cd6cde8e9d0104dcce80673d7ffe9958bfb71b7
Evan Plett <evanplett@gmail.com>
2020-03-15 00:39:58 +0000

    Fixing logging error

--- src/log_config.ts
+++ src/log_config.ts
@@ -7,7 +7,7 @@ import {
 } from 'typescript-logging';
 
 
-CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Trace));
+CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Warn));
 
 export const logArgumentEngineBase = new Category('argument_engine');
 
--- src/rest/argument_rest.ts
+++ src/rest/argument_rest.ts
@@ -17,7 +17,7 @@ export class Argument {
 
   // *********** CREATE ********** //
   async create(request: Request, response: Response, next: NextFunction): Promise<void> {
-    logger.trace('Rest::Argument::create(request.body: %o', request.body);
+    logger.trace('Rest::Argument::create(request.body: ' + request.body);
     this.af
     .createOne(request.body.conclusion, request.body.reasoning_method, request.body.premises)
     .then((value) => {
